86f46a6dde708a9fc55f05e91949e078ea0f9735
defalt11-code <royilao1114@gmail.com>
2025-10-02 18:04:13 +0800

    style the buttons

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const addBooksBtn = document.querySelector(".add-books");
 const formContainer = document.querySelector(".form-container");
 const overlay = document.querySelector("#overlay");
 
+overlay.addEventListener("click", () => {
+  overlay.classList.toggle("active")
+  formContainer.classList.toggle("active")
+
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+})
+
 addBooksBtn.addEventListener("click", () => {
   overlay.classList.toggle("active");
   formContainer.classList.toggle("active");
